7985c89325d52b6f80f2b866efb8b5b673765de8
hyejunseo <shyejun0718@naver.com>
2023-04-01 23:55:34 +0900

    add other models

--- api/models.py
+++ api/models.py
@@ -16,20 +16,20 @@ class BaseModel(models.Model):
 
 class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.TextField(max_length=60)
+    nickname = models.CharField(max_length=60)
     email = models.EmailField(max_length=60)
-    password = models.TextField(max_length=200)
+    password = models.CharField(max_length=200)
     profileImgPath = models.TextField(null=True)
     friends = models.ManyToManyField('self')
 
 
 class School(BaseModel):
-    name = models.TextField(max_length=60)
-    campus = models.TextField(max_length=60, null=True)
+    name = models.CharField(max_length=60)
+    campus = models.CharField(max_length=60, null=True)
 
 
 class Board(BaseModel):
-    name = models.TextField(max_length=60)
+    name = models.CharField(max_length=60)
     school = models.ForeignKey("School", related_name="school", on_delete=models.CASCADE)
     profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
 
@@ -37,8 +37,8 @@ class Board(BaseModel):
 class Post(BaseModel):
     board = models.ForeignKey("Board", related_name="board", on_delete=models.CASCADE)
     profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
-    title = models.TextField(max_length=100, null=True)
-    contents = models.TextField(max_length=200)
+    title = models.CharField(max_length=100, null=True)
+    contents = models.CharField(max_length=200)
     isAnonymous = models.CharField(max_length=10, default='Y')
     isQuestion = models.CharField(max_length=10, default='N')
 
@@ -52,4 +52,57 @@ class Comment(BaseModel):
     post = models.ForeignKey("Post", related_name="post", on_delete=models.CASCADE)
     profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, default=0)
-    contents = models.TextField(max_length=200)
+    contents = models.CharField(max_length=200)
+
+
+class PostLike(BaseModel):
+    post = models.ForeignKey("Post", related_name="post", on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
+
+
+class CommentLike(BaseModel):
+    comment = models.ForeignKey("Comment", related_name="post", on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
+
+
+class Scrap(BaseModel):
+    post = models.ForeignKey("Post", related_name="post", on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
+
+
+class TimeTable(BaseModel):
+    profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+
+
+class LectureDomain(BaseModel):
+    name = models.CharField(max_length=60)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, default=0)
+
+
+class Lecture(BaseModel):
+    lectureDomain = models.ForeignKey("LectureDomain", related_name="lectureDomain", on_delete=models.CASCADE, null=True)
+    collegeYear = models.CharField(max_length=10, default='0')
+    credit = models.CharField(max_length=10)
+    category = models.CharField(max_length=100)
+    professor = models.CharField(max_length=100)
+    lectureCode = models.CharField(max_length=60)
+    classRoom = models.CharField(max_length=100)
+    dayAndTime = models.CharField(max_length=100)
+
+
+class TakeLecture(BaseModel):
+    profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
+    lecture = models.ForeignKey("Lecture", related_name="lecture", on_delete=models.CASCADE)
+
+
+class LectureReview(BaseModel):
+    lecture = models.ForeignKey("Lecture", related_name="lecture", on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
+    rating = models.CharField(max_length=10)
+    contents = models.CharField(max_length=200)
+
+
+class ReviewLike(BaseModel):
+    lectureReview = models.ForeignKey("LectureReview", related_name="lectureReview", on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", related_name="profile", on_delete=models.CASCADE)
