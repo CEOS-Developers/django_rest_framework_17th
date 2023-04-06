a163370599fde2b0b2d3821ea8ed2983177c12f2
haen-su <haensuya@gmail.com>
2023-04-06 19:21:16 +0900

    [refactor] 각admin.py에 코드 추가

--- api/admin.py
+++ api/admin.py
@@ -1,23 +1,6 @@
 from django.contrib import admin
 
-from .models import *
-
 # Register your models here.
-
-admin.site.register(Profile)
-admin.site.register(School)
-admin.site.register(Board)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(CommentReply)
-admin.site.register(CommentLike)
-admin.site.register(CommentReplyLike)
-admin.site.register(PostLike)
-admin.site.register(Timetable)
-admin.site.register(Course)
-admin.site.register(CourseDetail)
-admin.site.register(Friend)
-admin.site.register(Scrap)
 
 
 
