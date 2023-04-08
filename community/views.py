bc0320b64c2e90ebbe3e260fc2479d911efda001
ImTakGyun <gyun1712@gmail.com>
2023-04-08 19:50:03 +0900

    Post CRUD API 구현

--- community/views.py
+++ community/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from .models import Post
 from .serializers import PostSerializer
 from django.http import Http404
 
@@ -9,9 +12,65 @@ from django.http import Http404
 # Create your views here.
 
 class PostList(APIView):
+    """
+    게시글 생성
+    /post/
+    """
+
     def post(self, request, format=None):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    """
+    게시글 조회
+    /post/
+    """
+
+    def get(self, request, format=None):
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    """
+    특정 게시물 조회
+    /post/{pk}/
+    """
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    """
+    특정 게시물 수정
+    /post/{pk}/
+    """
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    """
+    특정 게시물 삭제
+    /post/{pk}/
+    """
+
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
