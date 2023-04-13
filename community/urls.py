f592f3bbee0481e3c9252e4a456d222792ca4668
hyejunseo <shyejun0718@naver.com>
2023-04-13 22:44:30 +0900

    Refactor to ViewSet

--- community/urls.py
+++ community/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from community import views
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+# urlpatterns = [
+#     path('boards/', views.BoardList.as_view()),
+#     path('boards/<int:board_id>/', views.PostList.as_view()),
+#     path('posts/<int:post_id>/', views.PostDetail.as_view()),
+# ]
+
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register('boards', BoardViewSet)
 
 urlpatterns = [
-    path('boards/', views.BoardList.as_view()),
-    path('boards/<int:board_id>/', views.PostList.as_view()),
-    path('posts/<int:post_id>/', views.PostDetail.as_view()),
+    path('', include(router.urls))
 ]
--- community/views.py
+++ community/views.py
@@ -5,43 +5,54 @@ from rest_framework.response import Response
 from .serializers import *
 from .models import *
 from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets
 
 
-class BoardList(APIView):
-    def get(self, request, format=None):
-        boards = Board.objects.filter(status='A')
-        serializer = BoardSerializer(boards, many=True)
-        return Response(serializer.data)
+# class BoardList(APIView):
+#     def get(self, request, format=None):
+#         boards = Board.objects.filter(status='A')
+#         serializer = BoardSerializer(boards, many=True)
+#         return Response(serializer.data)
+#
+#
+# class PostList(APIView):
+#     def post(self, request, board_id, format=None):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save(board_id=board_id)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def get(self, request, board_id, format=None):
+#         posts = Post.objects.filter(board_id=board_id, status='A').order_by('-created_at')
+#         serializer = PostListSerializer(posts, many=True)
+#         return Response(serializer.data)
+#
+#
+# class PostDetail(APIView):
+#     def get_object(self, post_id):
+#         post = get_object_or_404(Post, pk=post_id)
+#         return post
+#
+#     def get(self, request, post_id, format=None):
+#         post = self.get_object(post_id)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     def put(self, request, post_id, format=None):
+#         post = self.get_object(post_id)
+#         serializer = PostSerializer(post, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class PostList(APIView):
-    def post(self, request, board_id, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(board_id=board_id)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def get(self, request, board_id, format=None):
-        posts = Post.objects.filter(board_id=board_id, status='A').order_by('-created_at')
-        serializer = PostListSerializer(posts, many=True)
-        return Response(serializer.data)
+class BoardViewSet(viewsets.ModelViewSet):
+    serializer_class = BoardSerializer
+    queryset = Board.objects.all()
 
 
-class PostDetail(APIView):
-    def get_object(self, post_id):
-        post = get_object_or_404(Post, pk=post_id)
-        return post
-
-    def get(self, request, post_id, format=None):
-        post = self.get_object(post_id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, post_id, format=None):
-        post = self.get_object(post_id)
-        serializer = PostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
