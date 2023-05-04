622a467566ee3e435f2abe3d5afc3be432019cc4
hyejun <shyejun0718@naver.com>
2023-05-04 19:08:53 +0900

    [Refact] Add soft-delete, edit custom filtering methods

--- community/filters.py
+++ community/filters.py
@@ -5,15 +5,30 @@ from .models import *
 class PostFilter(FilterSet):
     # board = filters.NumberFilter(field_name='board_id')
     board = filters.NumberFilter(method='filter_board')
-    profile = filters.NumberFilter(field_name='profile_id')
-    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
-    contents = filters.CharFilter(field_name='contents', lookup_expr='icontains')
+    # profile = filters.NumberFilter(field_name='profile_id')
+    profile = filters.NumberFilter(method='filter_profile')
+    # title = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    title = filters.CharFilter(method='filter_title')
+    # contents = filters.CharFilter(field_name='contents', lookup_expr='icontains')
+    contents = filters.CharFilter(method='filter_contents')
 
     class Meta:
         model = Post
         fields = ['board', 'profile', 'title', 'contents']
 
+    # def filter_board(self, queryset, board_id, value):
+    #     return queryset.filter(**{
+    #         board_id: value,
+    #     })
+
     def filter_board(self, queryset, board_id, value):
-        return queryset.filter(**{
-            board_id: value,
-        })
+        return queryset.filter(board_id=value, deleted_at__isnull=True)
+
+    def filter_profile(self, queryset, profile_id, value):
+        return queryset.filter(profile_id=value, deleted_at__isnull=True)
+
+    def filter_title(self, queryset, title, value):
+        return queryset.filter(title__icontains=value, deleted_at__isnull=True)
+
+    def filter_contents(self, queryset, contents, value):
+        return queryset.filter(contents__icontains=value, deleted_at__isnull=True)
