ae17fb44f2e4f73480acd1aae88d304828706e78
hyejun <shyejun0718@naver.com>
2023-05-05 20:37:31 +0900

    [Feat] Register, login, logout

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from account import views
+
+
+urlpatterns = [
+    path("register/", views.RegisterAPIView.as_view()),
+    path("login/", views.LoginAPIView.as_view()),
+    path("logout/", views.LogoutAPIView.as_view()),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+# 회원 가입
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = MyUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# 로그인
+class LoginAPIView(APIView):
+    def post(self, request):
+        # 유저 인증
+        user = authenticate(
+            email=request.data.get("email"), password=request.data.get("password")
+        )
+        # 해당 이메일, 비밀 번호로 가입한 유저가 있는 경우
+        if user is not None:
+            serializer = MyUserSerializer(user)
+            # jwt, refresh token 발급
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "Login success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            # refresh token 은 쿠키에 저장
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# 로그 아웃
+class LogoutAPIView(APIView):
+    def post(self, request):
+        # 클라이언트의 쿠키에 저장된 refresh 토큰을 삭제함으로써 로그 아웃 처리
+        response = Response({
+            "message": "Logout success"
+        }, status=status.HTTP_202_ACCEPTED)
+        response.delete_cookie('refresh')
+        return response
