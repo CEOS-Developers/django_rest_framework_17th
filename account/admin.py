4727078c7bf5ca7b2a9444c7349bcbe0620d1e1a
hyejun <shyejun0718@naver.com>
2023-05-05 18:52:00 +0900

    [Add] Create myUser serializer

--- account/admin.py
+++ account/admin.py
@@ -4,3 +4,4 @@ from account.models import *
 
 admin.site.register(Profile)
 admin.site.register(School)
+admin.site.register(MyUser)
--- account/serializers.py
+++ account/serializers.py
@@ -2,6 +2,26 @@ from rest_framework import serializers
 from account.models import *
 
 
+class MyUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyUser
+        fields = '__all__'
+
+    def create(self, validated_data):
+        email = validated_data.get('email')
+        nickname = validated_data.get('nickname')
+        school_id = validated_data.get('school_id')
+        password = validated_data.get('password')
+        user = MyUser(
+            email=email,
+            nickname=nickname,
+            school_id=school_id
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
