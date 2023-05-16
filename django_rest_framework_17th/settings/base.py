9feb51ecd5725e040d4fb7f7510be93987790c42
mirageoasis <kimhw0820@naver.com>
2023-05-17 00:09:56 +0900

    build : https test

--- django_rest_framework_17th/settings/base.py
+++ django_rest_framework_17th/settings/base.py
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 # Application definition
 
